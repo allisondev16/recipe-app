fb238bd6e32645e8b31f4a0c7ae06d04b9a3d11a
allisondev16 <allisonmamangun@gmail.com>
2022-01-19 21:41:21 +0800

    Add form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [searchText, setSearchText] = useState('');
+  const [array, setArray] = useState([]);
+
+  function handleChange(event) {
+    const searchText = event.target.value;
+    setSearchText(searchText);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="navbar">
+        <h1>Tasty</h1>
       </header>
+      <form>
+        <label>Find a Recipe <input type="text" onChange={handleChange}></input></label>
+
+        <input type="submit"></input>
+      </form>
+
     </div>
   );
 }
