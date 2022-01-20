298d58482c3a86c1c0200e0f21c7da826c3b69f1
allisondev16 <allisonmamangun@gmail.com>
2022-01-20 22:10:31 +0800

    Add API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import axios from 'axios';
+import Recipe from './components/recipe';
 
 function App() {
 
@@ -12,6 +14,30 @@ function App() {
     setSearchText(searchText);
   }
 
+  const options = {
+    method: 'GET',
+    url: 'https://tasty.p.rapidapi.com/recipes/list',
+    params: { from: '0', size: '20', tags: 'under_30_minutes' },
+    headers: {
+      'x-rapidapi-host': 'tasty.p.rapidapi.com',
+      'x-rapidapi-key': '639b5a8537msh53a9d6eb2b85039p180844jsn3986e4cf168e'
+    }
+  };
+
+  function fetchData() {
+    return new Promise((resolve, reject) => {
+      axios.request(options).then(function (response) {
+        resolve(response.data);
+      }).catch(function (error) {
+        reject(error);
+      });
+    });
+  }
+
+  const recipe = fetchData();
+  console.log('recipe', recipe);
+
+
   return (
     <div className="App">
       <header className="navbar">
@@ -22,6 +48,11 @@ function App() {
 
         <input type="submit"></input>
       </form>
+
+      {/* data.results.map(recipe => {
+        return <Recipe name={recipe.name}/>
+      }); */}
+
 
     </div>
   );
---
+++ src/components/recipe.js
@@ -0,0 +1,14 @@
+
+
+function Recipe(props) {
+
+
+    return (
+        <div>
+            <h2>{props.name}</h2>
+        </div>
+    );
+}
+
+
+export default Recipe;
