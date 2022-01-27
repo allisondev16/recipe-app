7f1cc812520b0617738c6982b97682c2895186fd
allisondev16 <allisonmamangun@gmail.com>
2022-01-27 22:06:08 +0800

    Change API and add react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Recipe from './components/recipe';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
 
@@ -17,8 +18,9 @@ function App() {
 
   function fetchData(data) {
     console.log(data.results);
+    const baseURI = data.baseUri;
     const recipesArray = data.results.map(result => {
-      return { name: result.name, thumbnail: result.thumbnail_url }
+      return { name: result.title, image: baseURI + result.image }
     });
     setRecipes(recipesArray);
   }
@@ -28,17 +30,23 @@ function App() {
 
     const options = {
       method: 'GET',
-      url: 'https://tasty.p.rapidapi.com/recipes/list',
-      params: { from: '0', size: '5', q: searchText },
+      url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search',
+      params: {
+        query: searchText,
+        instructionsRequired: 'true',
+        number: '10',
+        offset: '0',
+        type: 'main course'
+      },
       headers: {
-        'x-rapidapi-host': 'tasty.p.rapidapi.com',
-        'x-rapidapi-key': '639b5a8537msh53a9d6eb2b85039p180844jsn3986e4cf168e'
+        'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com',
+        'x-rapidapi-key': '639somethinge'
       }
     };
 
-    axios.request(options).then(response => {
+    axios.request(options).then(function (response) {
       fetchData(response.data);
-    }).catch(error => {
+    }).catch(function (error) {
       console.error(error);
     });
   }
@@ -54,18 +62,26 @@ function App() {
 
 
   return (
-    <div className="App">
-      <header className="navbar">
-        <h1>Tasty</h1>
-      </header>
-      <form>
-        <label>Find a Recipe <input type="text" onChange={handleChange}></input></label>
+    <Router>
+      <Routes>
+        <Route path="/" element={<header className="navbar">
+          <h1>Tasty</h1>
+        </header>}>
+          <Route index element={
+            <div>
+              <form>
+                <label>Find a Recipe <input type="text" onChange={handleChange}></input></label>
 
-        <input type="submit" onClick={handleSubmit}></input>
-      </form>
+                <input type="submit" onClick={handleSubmit}></input>
+              </form>
 
-      {recipes.map((recipe, index) => <Recipe key={index} name={recipe.name} image={recipe.thumbnail} />)}
-    </div>
+              {recipes.map((recipe, index) => <Recipe key={index} name={recipe.name} image={recipe.image} />)}
+            </div>
+          } />
+          <Route path="recipe" element={<h1>Hello</h1>} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
