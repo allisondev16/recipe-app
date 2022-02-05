11546aaa76ecdee51dd008551580c1e18f644669
allisondev16 <allisonmamangun@gmail.com>
2022-02-05 19:30:30 +0800

    Update formatting of instructions and added time

--- README.md
+++ README.md
@@ -39,6 +39,8 @@ console.log(test.split(/(?=[.?!])|(?<=[.?!])/g));
 ```
 
 - [I just learned that an ordered list needs padding to show the numbers.](https://www.sitepoint.com/community/t/ol-tag-not-displaying/5325)
+
+- JavaScript String replace()
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/RecipeDetails.js
+++ src/components/RecipeDetails.js
@@ -21,6 +21,7 @@ function RecipeDetails() {
 
         axios.request(options).then(function (response) {
             setInformation(response.data);
+            console.log('Recipe info', response.data);
         }).catch(function (error) {
             console.error(error);
         });
@@ -28,12 +29,18 @@ function RecipeDetails() {
 
     useEffect(() => {
         const instruction = information.instructions;
+        console.log('original', instruction);
 
-        const instructionsArray = instruction.split(/(?=[.?!])|(?<=[.?!])/g);
+        /*** Format the instruction into paragraphs ***/
+
+        const instructionsArray = instruction.split(/(?=[.])|(?<=[.])/g);
+        console.log('splitted', instructionsArray);
 
         for (let index = 1; index < instructionsArray.length; index++) {
-            if (instructionsArray[index][0] !== ")") {
-                // instructionsArray[index][1] = "<New Paragraph>";
+            if (instructionsArray[index][0] === ")") {
+                if (instructionsArray[index][1] !== " ") {
+                    instructionsArray[index] = instructionsArray[index].replace(")", ")<New Paragraph>");
+                }
             } else if (instructionsArray[index][0] !== " " && instructionsArray[index][0] !== ".") {
                 instructionsArray[index] = `<New Paragraph>${instructionsArray[index]}`;
             }
@@ -41,22 +48,39 @@ function RecipeDetails() {
 
         const joinInstructionsArray = instructionsArray.join("");
         const instructionsArrayFinal = joinInstructionsArray.split("<New Paragraph>");
-        console.log(instructionsArrayFinal);
+        console.log('final', instructionsArrayFinal);
         setInstructions(instructionsArrayFinal);
+
+        /*********************************************/
+
     }, [information])
 
 
     return (
         <div className='recipeDetails'>
-            <div className='recipeDetails__container'>
+            <div className='recipeDetails__title'>
                 <h2>{recipe.name}</h2>
                 <img src={recipe.image} alt={recipe.name}></img>
             </div>
-            <div className='recipeDetails__container'>
+            <div className='recipeDetails__time'>
+                <span>
+                    <span>Prep Time</span>
+                    <span>{information.cookingMinutes}</span>
+                </span>
+                <span>
+                    <span>Cook Time</span>
+                    <span>10</span>
+                </span>
+                <span>
+                    <span>Total Time</span>
+                    <span>20</span>
+                </span>
+            </div>
+            <div className='recipeDetails__ingredients'>
                 <h3>Ingredients:</h3>
                 {information.extendedIngredients.map((ingredient, index) => <p key={index}>{ingredient.original}</p>)}
             </div>
-            <div className='recipeDetails__container'>
+            <div className='recipeDetails__instructions'>
                 <h3>Instructions:</h3>
                 <ol>{instructions.map((instruction, index) => <li key={index}>{instruction}</li>)}</ol>
             </div>
