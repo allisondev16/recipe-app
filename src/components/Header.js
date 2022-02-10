c33aed4482d2b0bf4b0a0befc441b79d27d91d46
allisondev16 <allisonmamangun@gmail.com>
2022-02-10 22:27:16 +0800

    Add styles and mui icon to search form

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,10 +16,14 @@ function Header(props) {
             <header>
                 <nav className='container'>
                     <Link to="/"><h1>Tasty</h1></Link>
-                    <form>
-                        <input className='search-field' type="text" onChange={props.onChange} placeholder="Find a Recipe"></input>
+                    <form className='search'>
+                        <input className='search__input' type="text" onChange={props.onChange} placeholder="Find a Recipe"></input>
 
-                        <input type="submit" onClick={handleSubmit}></input><SearchIcon />
+                        <button className='search__icon' onClick={handleSubmit}><SearchIcon /></button>
+
+                        {/* <input type="submit" onClick={handleSubmit}></input> */}
+
+
                     </form>
                 </nav>
 
